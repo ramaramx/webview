1cfd01dd9cfed7ac6382ff367ae81986dd68bfac
ramaramx <rambudiarto@gmail.com>
2022-06-09 12:52:08 +0700

    new

--- main.js
+++ main.js
@@ -16,8 +16,8 @@ const preloadImages = () => {
 
 const img = new Image()
 img.src = currentFrame(1);
-canvas.width=1920;
-canvas.height=1080;
+canvas.width=window.innerWidth;
+canvas.height=window.innerHeight;
 img.onload=function(){
   context.drawImage(img, 0, 0);
 }
