5fc778a84d8479b8eea43b9f4be554b8064ae7c6
ramaramx <rambudiarto@gmail.com>
2022-06-10 16:35:11 +0700

    new

--- scroll.js
+++ scroll.js
@@ -3,15 +3,16 @@ gsap.registerPlugin(ScrollTrigger);
 let tl = gsap.timeline({scrollTrigger: {
     trigger: ".text-container",
     start: 0,
-    end: "+=5000", // stay pinned for 1000 pixels (make this whatever you want)
-    markers: true,
+    end: "+=7000", // stay pinned for 1000 pixels (make this whatever you want)
+    markers: {startColor: "#00000000", endColor: "#00000000", fontSize: "12px"},
     scrub: true,
     pin: true,
+    
   }});
 
 let stayTime = 2; // seconds between each text flip on the timeline (not literally seconds on screen - we're just spacing them out on the timeline)
 let textElements = gsap.utils.toArray(".text" ); // get an Array of all the ".text" elements
-let scrolltextElements = gsap.utils.toArray(".sticky-text-left")
+// let scrolltextElements = gsap.utils.toArray(".sticky-text-left")
 
 // loop through each text element and add an autoAlpha flip at the appropriate times on the timeline
 textElements.forEach((el, i) => {
@@ -23,11 +24,11 @@ textElements.forEach((el, i) => {
 // add some space at the end of the timeline so the last one stays for the correct duration before things get unpinned.
 tl.set({}, {delay: stayTime});
 
-scrolltextElements.forEach((el, i) => {
-    tl.set(el, {autoAlpha: 1}, i * stayTime);
-    if (i !== 0) { // if it's the first one, we don't need to toggle the previous one off.
-      tl.set(textElements[i - 1], {autoAlpha: 0}, i * stayTime);
-    }
-  });
-  // add some space at the end of the timeline so the last one stays for the correct duration before things get unpinned.
-  tl.set({}, {delay: stayTime});
+// scrolltextElements.forEach((el, i) => {
+//     tl.set(el, {autoAlpha: 1}, i * stayTime);
+//     if (i !== 0) { 
+//       tl.set(textElements[i - 1], {autoAlpha: 0}, i * stayTime);
+//     }
+//   });
+  
+//   tl.set({}, {delay: stayTime});
